134940152ee205693dee30be4f81342e2a74140b
Haneef Puttur <cdsputtur@gmail.com>
2016-08-11 19:43:20 +0400

    MVC - ASP.NET C#  Multi Language Demo
    Creating Multi language Website Using MVC .Net Resources and
    Globalizations

---
+++ Haneef/Haneef/Controllers/LangController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Haneef.Controllers
+{
+    public class LangController : Controller
+    {
+        //
+        // GET: /Lang/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Change(string lang)
+            {
+            if (lang != null)
+                {
+
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(lang);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(lang);
+
+                }
+            HttpCookie cookie = new HttpCookie("Language");
+            cookie.Value = lang;
+            Response.Cookies.Add(cookie);
+
+            return View("Index");
+            }
+
+    }
+}
